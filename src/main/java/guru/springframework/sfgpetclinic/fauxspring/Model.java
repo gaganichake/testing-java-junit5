0a6db65fc14da420da18c8fe67ab5779aa97a993
Gagan Ichake <gaganichake@yahoo.com>
2022-03-23 10:14:31 -0500

    Assignment - Write JUnit Test for Vet Controller List Vets

--- src/main/java/guru/springframework/sfgpetclinic/fauxspring/Model.java
+++ src/main/java/guru/springframework/sfgpetclinic/fauxspring/Model.java
@@ -1,8 +1,11 @@
 package guru.springframework.sfgpetclinic.fauxspring;
 
+import java.util.Map;
+
 public interface Model {
 
     void addAttribute(String key, Object o);
 
     void addAttribute(Object o);
+
 }
---
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/VetControllerTest.java
@@ -0,0 +1,42 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import guru.springframework.sfgpetclinic.fauxspring.Model;
+import guru.springframework.sfgpetclinic.fauxspring.ModelMapImpl;
+import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.services.SpecialtyService;
+import guru.springframework.sfgpetclinic.services.VetService;
+import guru.springframework.sfgpetclinic.services.map.SpecialityMapService;
+import guru.springframework.sfgpetclinic.services.map.VetMapService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VetControllerTest {
+
+    VetService vetService;
+    SpecialtyService specialtyService;
+    VetController controller;
+
+    @BeforeEach
+    void setUp() {
+        specialtyService = new SpecialityMapService();
+        vetService = new VetMapService(specialtyService);
+        controller = new VetController(vetService);
+
+        vetService.save(new Vet(1L, "joe", "buck", null));
+        vetService.save(new Vet(2L, "Jim", "Smith", null));
+    }
+
+    @Test
+    void testListVets() {
+        Model model = new ModelMapImpl();
+        assertEquals("vets/index", controller.listVets(model));
+
+        Set modelAttribute = (Set)((ModelMapImpl)model).getMap().get("vets");
+
+        assertEquals(modelAttribute.size(), 2);
+    }
+}
