c171a9ef82a8339fae1f8093a3df5e632388402e
Gagan Ichake <gaganichake@yahoo.com>
2022-03-22 19:09:30 -0500

    Conditional JUnit Test Execution

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.*;
 
 import java.io.FileNotFoundException;
 import java.time.Duration;
@@ -71,5 +72,41 @@ class IndexControllerTest {
     void testAssumptionQATrue(){
 
         assumeTrue("QA".equalsIgnoreCase("QA"));
+    }
+
+    @EnabledOnOs(OS.MAC)
+    @Test
+    void testMeOnMacOS(){
+
+    }
+
+    @EnabledOnOs(OS.WINDOWS)
+    @Test
+    void testMeOnWindows(){
+
+    }
+
+    @EnabledOnJre(JRE.JAVA_8)
+    @Test
+    void testMeOnJava8(){
+
+    }
+
+    @EnabledOnJre(JRE.JAVA_11)
+    @Test
+    void testMeOnJava11(){
+
+    }
+
+    @EnabledIfEnvironmentVariable(named = "USER", matches = "jt")
+    @Test
+    void testIfUserJT(){
+
+    }
+
+    @EnabledIfEnvironmentVariable(named = "USER", matches = "gaganichake")
+    @Test
+    void testIfUserGagan(){
+
     }
 }
