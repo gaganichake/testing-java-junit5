fedff530fa8da7f51fac6e2eb02a6e52911ad0d4
Gagan Ichake <gaganichake@yahoo.com>
2022-03-23 12:11:24 -0500

    Using JUnit Default Test Methods

--- src/test/java/guru/springframework/sfgpetclinic/ControllerTests.java
+++ src/test/java/guru/springframework/sfgpetclinic/ControllerTests.java
@@ -1,7 +1,16 @@
 package guru.springframework.sfgpetclinic;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInstance;
 
 @Tag("controller")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public interface ControllerTests {
+
+    // Try to run this without @TestInstance configured. @BeforeAll method must be static.
+    @BeforeAll
+    default void setUpAll() {
+        System.out.println("Lets do something here Before All test run under controller. This default Interface method can be useful");
+    }
 }
